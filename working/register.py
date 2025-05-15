8c0cd99b6e88f9b9e3e55c4c17dda11612166e89
Prakash Kumar Chaudhary <prakashkchaudhary5209@gmail.com>
2025-05-15 13:30:31 +0545

    login persistent

--- working/register.py
+++ working/register.py
@@ -1,8 +1,8 @@
 import requests
+import jwt
 
 
 class Register:
-
     def __init__(self, email, username, password, confirm_password):
         self.email = email
         self.username = username
@@ -10,7 +10,7 @@ class Register:
         self.confirm_password = confirm_password
 
     # register account
-    def register_new_account(self):
+    def __register_new_account(self):
         if self.confirm_password == self.confirm_password:
             try:
                 test_response = requests.post("http://localhost:8000/create-user/",
@@ -19,6 +19,30 @@ class Register:
                                                   'username': self.username,
                                                   'password': self.password
                                               })
+                return test_response.status_code
             except Exception as e:
                 print(e)
-            print("response", test_response)
+
+
+class Login:
+    secret = "prakashchaudhary"
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def get_user(self):
+        try:
+            response = requests.post("http://localhost:8000/get-user/",
+                                     json={"username": self.username, "password": self.password})
+            return response
+        except Exception as e:
+            return e
+
+    def makePayload(self):
+        resp = self.get_user()
+        return resp
+
+    # def generateToken(self):
+    #     en = jwt.encode(self.getPayload(), Login.secret, algorithm=["HS256"])
+    #     print("token", en)
