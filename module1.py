8ab4e92f86c91170f28e5b6890cc166759f4da82
Prakash Kumar Chaudhary <prakashkchaudhary5209@gmail.com>
2025-06-18 15:26:31 +0545

    again cleaning

--- module1.py
+++ module1.py
@@ -2,17 +2,6 @@ import numpy as np
 
 # random 200 words generator
 
-
-# def typing_test_words():
-#     with open("words_alpha.txt", "r") as word:
-#         re = word.read()
-#         lst = re.split()
-#         npary = np.array([i for i in lst if len(i) <= 6])
-#         random_words = np.random.choice(npary,size=30)
-#         print(random_words)
-#         return " ".join(random_words)
-
-
 def typing_test_words():
     with open("TestTypingWords","r") as word:
         rawlst = [i.split() for i in word]
